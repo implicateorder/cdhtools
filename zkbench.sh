a1418c18425830b8d1ab81b1f95f74b785f5c073
Dwai Lahiri <dlahiri@cloudera.com>
2016-11-01 15:19:59 -0500

    Updated benchit.sh with Apache License Added zkbench.sh for rudimentary zk load testing

--- zkbench.sh
+++ zkbench.sh
@@ -201,61 +201,9 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-indir="$(/usr/bin/uuidgen)"
-outdir="$(/usr/bin/uuidgen)"
-repdir="$(/usr/bin/uuidgen)"
+zk_conn_str=localhost:2181	#Add more as comma separated entries here
+scale=1
+/usr/bin/load_gen ${zk_conn_str} /load_gen ${scale} 2>/tmp/log.txt # run for 1 minute, then kill
+wc -l /tmp/log.txt
+load_gen ${zk_conn_str} /load_gen clean
 
-printUsage() {
- echo "Usage: ./$0 -d <data size in 100byte increments> -m <number of mappers> -r <number of reducers> -I <number of iterations> | -h 
-	d - data size in 100 byte increments. Defaults to 100GB
-	m - number of mappers - defaults to 40
-	r - number of reducers - defaults to 40
-	I - number of times to run the benchmarks
-	h - print this message\n";
-}
-
-while getopts d:m:r:I:h switch
-do
-    case $switch in
-	d) data_size=$OPTARG;;
-	m)map_tasks=$OPTARG;;
-	r)reduce_tasks=$OPTARG;;
-	I)iterations=$OPTARG;;
-	h) printUsage && exit 0;;
-	*) printUsage && exit 1;;
-    esac
-done
-shift $(( $OPTIND - 1 ))
-
-if [ -z $data_size ]; then
-     data_size="1000000000";
-fi
-if [ -z $map_tasks ]; then
-    map_tasks="40";
-fi
-if [ -z $reduce_tasks ]; then
-    reduce_tasks="40";
-fi
-if [ -z $iterations ]; then
-    iterations="20";
-fi
-
-# Change the number mappers, reducers, size etc here
-# The directories are generated using uuidgen
-
-bmarks="teragen terasort teravalidate"
-# First run has to be a teragen
-
-./terabench.sh -p teragen -x 3 -d $data_size -m $map_tasks -r $reduce_tasks -i $indir -o $outdir -O $repdir
-
-for i in $(seq $iterations)
-do
-    # Randomize which benchmark to run
-    for bm in $bmarks
-    do
-        runthis="$(echo $RANDOM $bm|sort -n|awk '{print $2}'|head -1)"
-	./terabench.sh -p $runthis -x 3 -d $data_size -m $map_tasks -r $reduce_tasks -i $indir -o $outdir -O $repdir
-	# sleep for random period of time before running the next benchmark
-	sleep $(( $RANDOM % 900 ))
-    done  
-done
